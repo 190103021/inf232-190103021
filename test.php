020cd36f74af5a3056088bb8a574bf2619a0cfd5
Assel Yembergenova <190103021@stu.sdu.edu.kz>
2021-02-07 22:52:54 +0600

    Logo

---
+++ test.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Third task</title>
+  </head>
+
+  <body>
+  	<?php
+
+    echo "This is my logo!!!<br>";
+     
+    ?>
+
+    <style>
+      h1{
+        text-align:center;
+        color:rgb(255,55,0);
+        word-spacing: 8px;
+        letter-spacing: 10px;
+        font-size:50px;
+        margin-left: 55px;
+      }
+    </style>
+     <svg width="1500" height="690">
+      <defs>
+       <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1;" />
+        <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+       </linearGradient>
+     </defs>
+       <polygon points="1040,345 1000,395 710,395 760,345" fill="yellow" />
+       <polygon points="1000,400 960,450 670,450 720,400" fill="yellow" />
+       <polygon points="960,455 920,505 630,505 680,455" fill="yellow" />
+       <polygon points="920,510 880,560 590,560 640,510" fill="yellow" />
+
+       <polygon points="160,345 200,395 490,395 440,345" fill="yellow" />
+       <polygon points="200,400 240,450 670,450 720,400" fill="yellow" />
+       <polygon points="240,455 280,505 630,505 680,455" fill="yellow" />
+       <polygon points="280,510 320,560 590,560 640,510" fill="yellow" />
+
+       <circle cx = "600" cy="500" r= "160" fill="url(#grad1)"/>
+       <circle cx = "600" cy="500" r= "125" fill="white" />
+       <circle cx = "600" cy="500" r= "90" fill="url(#grad1)"/>
+       <circle cx = "600" cy="500" r= "55" fill="white" />
+
+       <rect width="30" height="15" x="515" y="470" fill="url(#grad1)" stroke="white" stroke-width="4" />
+       <polygon points = "534,370 600,550 550,550" fill="url(#grad1)" stroke="white" stroke-width="4"/>
+       <polygon points = "534,370 470,550 520,550" fill="url(#grad1)" stroke="white" stroke-width="4"/>
+       
+       <polygon points="580,460 630,460 660,530" fill="url(#grad1)" stroke="white" stroke-width="4"/>
+       <polygon points="680,460 730,460 660,530" fill="url(#grad1)" stroke="white" stroke-width="4"/>
+       <polygon points="660,530 680,630 640,630" fill="url(#grad1)" stroke="white" stroke-width="4"/>
+
+     </svg>
+
+     <h1><i>Assel Yembergenova </i></h1>
+
+  </body>
+
+</html>
