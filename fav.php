ef5201dea56676a98ad2e8da047d560a28d0aa51
Assel Yembergenova <190103021@stu.sdu.edu.kz>
2021-02-07 22:03:54 +0600

    My favorite groups

---
+++ fav.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$groups = array("BTS", "EXO", "GOT7", "MonstaX", "Blackpink", "Twice");
+  echo "Number of my favorite groups: " . count($groups);
+  
+  echo  ".    " . $groups[0] . ", " . $groups[1] . ", " . $groups[2] . ", " . $groups[3] . ", " . $groups[4] . " and " . $groups[5];
+?>
+
+</body>
+</html>
